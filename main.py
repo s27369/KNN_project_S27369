8c14ede73dfa3a920ffd71dfe1f707b8f90b0f6a
Adam Dudkiewicz <dudkiewiczadam653@gmail.com>
2024-03-11 12:55:43 +0100

    imported math; added is_float() function modified str_to_dict() so that it's universal in terms of number of attributes added dataset_info() for initial dataset analysis added get_difference() to calculate the difference between individual vectors added get_observation()

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
-
+import math
 def read_file(path):
     with open(path, 'r') as f:
         file = f.read()
     return file
 
+def is_float(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
 def str_to_dict(string):
-    col_1="a"
-    col_2="b"
-    col_3="c"
-    col_4="d"
-    col_5="result"
     string = string.replace(",", ".")
     l = string.split()
     l = [x.strip() for x in l]
-    data = {col_1:[], col_2:[], col_3:[], col_4:[], col_5:[]}
-    for i in range(0, len(l), 5):
-        data[col_1].append(float(l[i]))
-        data[col_2].append(float(l[i+1]))
-        data[col_3].append(float(l[i+2]))
-        data[col_4].append(float(l[i+3]))
-        data[col_5].append(l[i+4])
+
+    num_attributes = 0
+    temp = l[num_attributes]
+    att_names =[]
+    while is_float(temp):
+        num_attributes+=1
+        att_names.append(f'att{num_attributes}')
+        temp = l[num_attributes]
+    att_names.append("result")
+
+    data = {x:[] for x in att_names}
+    # print(num_attributes)
+    for i in range(0, len(l), num_attributes+1):
+        for j in range(0, num_attributes):
+            data[att_names[j]].append(float(l[i+j]))
+        data[att_names[-1]].append(l[i+num_attributes])
+
+
+
     return data
 
 def file_to_dict(path, check=False):
@@ -27,20 +42,37 @@ def file_to_dict(path, check=False):
     dic = str_to_dict(f)
     if check:
         print(f'{path}:')
-        for k, v in dic.items(): print(f'{k}: {len(v)}')
+        dataset_info(dic)
         print()
     return dic
+
+def dataset_info(dic):
+    for k, v in list(dic.items())[:-1]: print(f'{k}: {len(v)} values, ranging from {min(v)} to {max(v)}')
+    s = set(dic["result"])
+    print(f'result: {len(s)} unique values {s}')
 
 def get_range(list):
     return max(list)-min(list)
 
+def get_difference(observation, data): #returns a single float value representing the difference of one observation to another
+    differences = [math.pow(observation[x]-data[x], 2) for x in range(len(observation)-1)]
+    return math.sqrt(sum(differences))
+
+def get_observation(dataset, index):
+    return [dataset[x][index] for x in dataset]
+
 if __name__ == '__main__':
-    data = file_to_dict("iris_training.txt", True)
+
+
+
+    train = file_to_dict("iris_training.txt", True)
     test = file_to_dict("iris_test.txt", True)
 
-    obs = [test[k][0] for k in test]
+    # k=int(input("k:"))
+    k = 3
 
-    val = (obs[0]-data[list(data.keys())[0]][0])/get_range(data[list(data.keys())[0]])
+    d = get_observation(train, 0)
+    obs = get_observation(test, 0)
 
-    print(val)
+    print(get_difference(obs, d))
 
